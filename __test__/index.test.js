064e2157973e9c6499bf190df6ecdbca378bf3b8
JasperHX <jasper-hendrikx@hotmail.com>
2021-09-06 22:46:09 +0200

    huiswerk

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,3 +1,51 @@
-test("controleer of de test wel werkt",  () => {
-    console.log("Ik word uitgevoerd")
+function add(num1, num2)  {
+    return num1 + num2;
+}
+
+
+test("The add function adds two numbers and returns the sum",  () => {
+    // Arrange
+
+    const number1 = 5;
+    const number2 = 10;
+
+    // ACT
+    const sum = add(number1, number2);
+
+    // Assert
+    expect(sum).toBe(15)
+
 })
+
+function findByName(names, user) {
+    // 1. itereer over de array heen met een for-loop
+    // 2. vergelijk de name van iedere entry met de user die we moeten vinden
+    // 3.  als de namen overeenkomen, moet het gehele object teruggegeven worden
+    for (let i = 0; i < names.length; i++) {
+        const currentUser = names[i];
+
+        if (currentUser.name === user)  {
+            return currentUser;
+        }
+    }
+}
+
+test("findByName will return an object from an array with the specified name", () => {
+
+})
+
+// Arrange
+const usernames = [
+    {name: "Piet", id: 1},
+    {name: "Klaas", id: 2},
+    {name: "Henk", id: 3},
+];
+
+const userToFind = "Klaas";
+// Act
+const result = findByName(usernames, userToFind)
+// Assert
+
+expect(result).toEqual({name: "Klaas", id: 2});
+
+test(name: findByName())
